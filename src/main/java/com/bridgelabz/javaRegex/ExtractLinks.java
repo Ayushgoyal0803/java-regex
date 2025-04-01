4f88050436c8ac8e465d36ad7034aaa064cf092d
Ayush <ayushgoyal0808@gmail.com>
2025-04-01 14:20:44 +0530

    [Ayush Goyal] Add: Program to find all links in a text using regex.

---
+++ src/main/java/com/bridgelabz/javaRegex/ExtractLinks.java
@@ -0,0 +1,20 @@
+package com.bridgelabz.javaRegex;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ExtractLinks {
+    public static void main(String[] args) {
+        String s1 = "Visit https://www.google.com and http://example.org for more info.";
+        String regex = "https?://(?:www\\.)?[A-Za-z0-9-_]+\\.[a-z]{2,}";
+
+        Pattern pattern = Pattern.compile(regex);
+
+        Matcher matcher = pattern.matcher(s1);
+
+        while (matcher.find()){
+            System.out.println("link found: " + matcher.group());
+        }
+
+    }
+}
